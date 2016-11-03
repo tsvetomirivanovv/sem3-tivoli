1eb2c6856909714a405d67e278ea6ad75598544f
Tsvetomir Ivanov <tsvetomirivanovv@gmail.com>
2016-11-03 15:01:28 +0100

    Created functionality for inserting the shift into the databse, sanitation, and requiring all inputs to be filled before submiting the form

---
+++ app/core/functions/shifts/shifts.php
@@ -0,0 +1,62 @@
+<?php
+
+// GET DB CONNECTION
+require '../../database/connect.php';
+
+// CHECK IF THERE IS ERROR
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+// CHECK FOR INPUT
+if (isset(
+    $_POST['shiftTitle'],
+    $_POST['shiftBeginDate'],
+    $_POST['shiftEndDate'],
+    $_POST['shiftClosingDate'],
+    $_POST['shiftDutyManager'],
+    $_POST['shiftCategory'],
+    $_POST['shiftParticipants']
+
+
+)){
+    // ASSIGN INPUT TO VARIABLES
+    $nonSanitizedTitle = $_POST['shiftTitle'];
+    $nonSanitizedDutyManager = $_POST['shiftDutyManager'];
+    $nonSanitizedCategory = $_POST['shiftCategory'];
+
+    // SANITIZE SPECIFIC VARIABLES
+    $sanitizedTitle = sanitizeString($conn,$nonSanitizedTitle);
+    $sanitizedDutyManager = sanitizeString($conn,$nonSanitizedDutyManager);
+    $sanitizedCategory = sanitizeString($conn,$nonSanitizedCategory);
+
+    $beginDate = $_POST['shiftBeginDate'];
+    $endDate = $_POST['shiftEndDate'];
+    $closingDate = $_POST['shiftClosingDate'];
+    $participants = $_POST['shiftParticipants'];
+
+
+    // BUILD QUERY
+    $query = "INSERT INTO shifts VALUES (NULL , '$sanitizedTitle', '$beginDate', '$endDate', '$closingDate', '$sanitizedDutyManager', '$sanitizedCategory', '$participants')";
+
+    // EXECUTES QUERY
+    $result = $conn->query($query);
+
+
+}
+
+// FUNCTION TO PREVENT HACKING
+function sanitizeString($conn,$var)
+{
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    $var = stripslashes($var);
+    $var = mysqli_real_escape_string($conn,$var);
+    return $var;
+}
+
+
+
+
+
+
--- app/views/shifts/createShift.php
+++ app/views/shifts/createShift.php
@@ -1,38 +1,40 @@
-<form method="post" action="createShift.php">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<form method="post" id="insertFormID" action="../../core/functions/shifts/shifts.php">
 
     <div class="label-input-container">
         <label>Title</label>
-        <input type="text" maxlength="25" name="shiftTitle">
+        <input type="text" maxlength="200" name="shiftTitle" required>
     </div>
 
     <div class="label-input-container">
         <br><label>Begin</label>
-        <input type="text" name="shiftBeginDate">
+        <input type="datetime-local" name="shiftBeginDate" required>
     </div>
 
     <div class="label-input-container">
         <br><label>End</label>
-        <input type="datetime-local" name="shiftEndDate">
+        <input type="datetime-local" name="shiftEndDate" required>
     </div>
 
     <div class="label-input-container">
         <br><label>Closing Booking</label>
-        <input type="datetime-local" name="shiftClosingDate">
+        <input type="datetime-local" name="shiftClosingDate" required>
     </div>
 
     <div class="label-input-container">
         <br><label>Duty Manager</label>
-        <input type="text" maxlength="20" name="shiftDutyManager">
+        <input type="text" maxlength="20" name="shiftDutyManager" required>
     </div>
 
     <div class="label-input-container">
         <br><label>Category</label>
-        <input type="text" maxlength="15" name="shiftCategory">
+        <input type="text" maxlength="15" name="shiftCategory" required>
     </div>
 
     <div class="label-input-container">
         <br><label>Number of Participants</label>
-        <input type="number" max="60" name="shiftParticipants">
+        <input type="number" minlength="1" maxlength="3" max="100" name="shiftParticipants" required>
     </div>
 
     <div class="submit">
