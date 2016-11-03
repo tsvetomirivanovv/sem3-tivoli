79f3ec36730099bc7d9e28f998c2730a13d55c2a
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-03 11:20:22 +0100

    refactor: update app structure

--- index.php
+++ index.php
@@ -1,11 +1,56 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Homepage - Tivoli app</title>
 
-echo 'Login page';
-echo "<br>";
-echo 'Petru was here!';
-echo "<br>";
-echo 'Dan remember to put semicolon after the statements';
-echo "<br>";
-echo "Hi there <br>";
-echo "Fuck you, Kay! <br>";
-?>
+        <link href="../vendor/Flat-UI/dist/css/flat-ui.min.css" rel="stylesheet">
+        <link href="../vendor/Flat-UI/dist/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+        <?php require 'views/shared/menu.php'; ?>
+
+        <!-- CONTENT -->
+        <div class="container">
+            <div class="row">
+
+                <h2>content</h2>
+
+                <?php
+                    $loggedin = false;
+
+                    if($loggedin) {
+                        require 'views/shifts/shifts.php';
+                    } else {
+                        require 'views/login/login.php';
+                    }
+                ?>
+
+                <div class="col-xs-3">
+                    <h2>sidebar</h2>
+
+                    <?php
+
+                        require 'core/init.php';
+
+                        $result = $conn->query("SELECT * FROM tmp_users");
+
+                        print '<pre>';
+                        print_r(mysqli_fetch_all($result));
+
+
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <!-- footer -->
+        <!-- <div class="container">
+            <div class="row">
+
+            </div>
+        </div> -->
+    </body>
+</html>
--- views/login/login.php
+++ views/login/login.php
@@ -1,5 +1,5 @@
 <div class="col-xs-9">
     You need to log in
 
-    <a href="/app/register.php">Click to register</a>
+    <a href="/register.php">Click to register</a>
 </div>
