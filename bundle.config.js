83b723e2fcfe43b3e9a417431dcc53f728bedf31
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-05 19:49:44 +0100

    refactor: change vendor (vendor0) folder name with bundle because of heroku name limitation

--- bundle.config.js
+++ bundle.config.js
@@ -2,9 +2,9 @@ module.exports = {
   bundle: {
     vendor: {
       scripts: [
-          './vendor/bootstrap/dist/js/bootstrap.js',
-          './vendor/flat-ui//dist/js/flat-ui.js',
-          './vendor/jquery/dist/jquery.js'
+          './bundle/bootstrap/dist/js/bootstrap.js',
+          './bundle/flat-ui//dist/js/flat-ui.js',
+          './bundle/jquery/dist/jquery.js'
       ],
       options: {
         rev: false
--- index.php
+++ index.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <title>Homepage - Tivoli app</title>
 
-        <link href="../vendor0/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../vendor0/flat-ui/dist/css/flat-ui.min.css" rel="stylesheet">
+        <link href="../bundle/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../bundle/flat-ui/dist/css/flat-ui.min.css" rel="stylesheet">
         <link href="../dist/app.min.css" rel="stylesheet">
         <script src="./dist/vendor.js" charset="utf-8"></script>
 
