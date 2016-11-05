c570af4b0e2fc296e7052b0f147f34cca9a41928
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-05 18:36:09 +0100

    refactor: removed content code from index header

--- app/views/shared/index-header.php
+++ app/views/shared/index-header.php
@@ -4,20 +4,3 @@
 <body>
 
 <?php include 'views/shared/menu.php'; ?>
-
-<!-- CONTENT -->
-<div class="container">
-    <div class="row">
-        <?php include 'views/site-description/description.php'; ?>
-        <?php
-        if (logged_in() === true){
-            include 'views/login/logged-in.php';
-        }else {
-            include 'views/login/login.php';
-        }
-        ?>
-
-    </div>
-
-</div>
-
