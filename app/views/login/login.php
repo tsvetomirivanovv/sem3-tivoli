2c240dd102ad14fe4d8668aeebd678d73510d9d7
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-05 18:45:16 +0100

    refactor: remove <form> tag (will use ajax), add ids for inputs

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -1,28 +1,23 @@
 <div class="col-xs-3">
     <div class="login-form">
-        <form action="login.php" method="post">
-        <div class="form-group">
-            <input type="text" class="form-control login-field" value="" placeholder="Username" name="login-name" />
-            <label class="login-field-icon fui-user" for="login-name"></label>
-        </div>
+            <div class="form-group">
+                <input type="text" class="form-control login-field" value="" placeholder="Username" id="login-name" name="login-name" />
+                <label class="login-field-icon fui-user" for="login-name"></label>
+            </div>
 
-        <div class="form-group">
-            <input type="password" class="form-control login-field" value="" placeholder="Password" name="login-pass" />
-            <label class="login-field-icon fui-lock" for="login-pass"></label>
-        </div>
+            <div class="form-group">
+                <input type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" name="login-pass" />
+                <label class="login-field-icon fui-lock" for="login-pass"></label>
+            </div>
 
-        <button class="btn btn-primary btn-lg btn-block login-button" type="submit" href="login.php">Log in</button>
-        <a class="login-link" href="#">Lost your password?</a>
-        </form>
+            <button class="btn btn-primary btn-lg btn-block login-button" type="submit">Log in</button>
+            <a class="login-link" href="#">Lost your password?</a>
     </div>
 </div>
-
-
 
 <script type="text/javascript">
 
     $('.login-button').click(function() {
         $.growl.error({ title: "Error", message: "<?php print_r($errors[0]); ?>" });
     });
-
 </script>
