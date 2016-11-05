068211fc62dd7a19a67739fb23a9cd350bfb09e0
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-05 18:46:21 +0100

    feat: add ajax request to ‘core/functions/login/login.php’ endpoint and based on the response display the right type of growl notification + message

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -18,6 +18,22 @@
 <script type="text/javascript">
 
     $('.login-button').click(function() {
-        $.growl.error({ title: "Error", message: "<?php print_r($errors[0]); ?>" });
+        $.ajax('core/functions/login/login.php', {
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                login_name: $('#login-name').val(),
+                login_pass: $('#login-pass').val()
+            }
+        })
+        .done(function(response) {
+            if(response.success) {
+                $.growl.notice({ title: "Success", message: response.message });
+                // delay with 5s to let the notification be displayed
+                location.reload().delay(5000);
+            } else {
+                $.growl.error({ title: "Error", message: response.message });
+            }
+        })
     });
 </script>
