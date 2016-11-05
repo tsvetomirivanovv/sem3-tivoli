403a0f816607ad7e95b4c76387c0f2fa2663ede9
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-05 18:42:57 +0100

    refactor: add bootstrap grid columns to logged-in template

--- app/views/login/logged-in.php
+++ app/views/login/logged-in.php
@@ -1,7 +1,9 @@
-<h2>Hello, <?php echo $user_data['first_name']; ?>!</h2>
-<ul>
-    <li>
-        <a href="logout.php">Log out</a>
-    </li>
+<div class="col-xs-3">
+    <h2>Hello, <?php echo $user_data['first_name']; ?>!</h2>
+    <ul>
+        <li>
+            <a href="logout.php">Log out</a>
+        </li>
 
-</ul>
+    </ul>
+</div>
