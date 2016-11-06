82893c7154495e3116d949b6f662803084ace8fe
teo9898@abv.bg <teo9696@abv.bg>
2016-11-06 17:49:59 +0100

    refactor: old notification files deleted, register content deleted, conditional statements changed,

--- app/core/functions/login/login.php
+++ app/core/functions/login/login.php
@@ -1,21 +1,20 @@
 <?php
 include '../../init.php';
 
-if (empty($_POST) === false) {
+if (!empty($_POST)) {
     $username = $_POST['login_name'];
     $password = $_POST['login_pass'];
-
-    if (empty($username) === true) {
+    if (empty($username)) {
         $errors = 'You need to enter a username';
-    } else if (empty($password) === true) {
+    } else if (empty($password)) {
         $errors = 'You need to enter a password';
-    } else if (user_exists($username) === false) {
+    } else if (!user_exists($username)) {
         $errors = 'Username not found';
-    } else if (user_active($username) === false) {
+    } else if (!user_active($username)) {
         $errors = 'You haven\'t activated your account';
     } else {
         $login = login($username, $password);
-        if ($login === false) {
+        if (!$login) {
             $errors = 'That username/password combination is incorrect';
         } else {
             $_SESSION['user_id'] = $login;
--- app/core/init.php
+++ app/core/init.php
@@ -5,10 +5,10 @@
     require 'functions/general.php';
     require 'functions/users.php';
 
-    if (logged_in() === true){
+    if (logged_in()){
         $session_user_id = $_SESSION['user_id'];
         $user_data = user_data($session_user_id, 'user_id', 'username', 'password', 'first_name', 'last_name', 'email', 'phone', 'address', 'zip_code', 'city', 'cv', 'profile_picture');
-        if (user_active($user_data['username']) === false){
+        if (!user_active($user_data['username'])){
             session_destroy();
             header('location: index.php');
             exit();
