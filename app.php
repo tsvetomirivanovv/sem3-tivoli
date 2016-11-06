d08ab3cd6e8859e5dbb2e84392be82b3a82c3f2a
teo9898@abv.bg <teo9696@abv.bg>
2016-11-06 21:29:17 +0100

    refactor: test mysqli comment clear up

--- app.php
+++ app.php
@@ -1,9 +1,4 @@
 <?php
-/*if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    echo 'We don\'t have mysqli!!!';
-} else {
-    echo 'Phew we have it!';
-}*/
     error_reporting(E_ALL);
     require 'app/core/init.php';
 
