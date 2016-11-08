b19527dc34e8f1af34d7226ac2f54a0a212a6904
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-08 21:42:55 +0100

    refactor: removed button and added the shifts list on grid

--- app/views/shifts/shiftCards.php
+++ app/views/shifts/shiftCards.php
@@ -1,12 +1,6 @@
 <div class="col-xs-9">
-    You are logged in so you'll see some shifts
-</div>
-<button id="viewShiftsButtonID">View all Shifts!</button>
-
-<div id="shiftCardsConteiner">
-    <div id="shiftContainer">
+    <div id="shiftCardsConteiner">
+        <div id="shiftContainer">
+        </div>
     </div>
 </div>
-
-
-
