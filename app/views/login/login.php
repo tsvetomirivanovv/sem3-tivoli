3b0d45431dcaf3681681fa95021810b396b2a8e4
teo9898@abv.bg <teo9696@abv.bg>
2016-11-08 10:36:54 +0100

    refactor: name properties in html renamed to avoid confusion

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -1,12 +1,12 @@
 <div class="col-xs-3">
     <div class="login-form">
             <div class="form-group">
-                <input type="text" class="form-control login-field" value="" placeholder="Username" id="login-name" name="login-name" />
+                <input type="text" class="form-control login-field" value="" placeholder="Username" id="login-name" name="login_name" />
                 <label class="login-field-icon fui-user" for="login-name"></label>
             </div>
 
             <div class="form-group">
-                <input type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" name="login-pass" />
+                <input type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" name="login_pass" />
                 <label class="login-field-icon fui-lock" for="login-pass"></label>
             </div>
 
