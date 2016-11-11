1426dde0c9f919866703a685082169a60140dd22
teo9898@abv.bg <teo9696@abv.bg>
2016-11-11 21:17:33 +0100

    refactor: added properties to the table rows

--- assets/js/app.js
+++ assets/js/app.js
@@ -124,7 +124,7 @@ $(document).ready(function () {
                     $.growl.notice({title: "Success", message: response.message});
                     // delay with 5s to let the notification be displayed
                     window.setTimeout(function () {
-                            window.location.href = "";
+                        window.location.href = "";
                     }, 3000);
                 } else {
                     $.growl.error({title: "Error", message: response.message});
@@ -245,4 +245,45 @@ $(document).ready(function () {
 
 
 
+    $.ajax({
+        type: "POST",
+        url: 'core/functions/login/view-all-accounts.php',
+        dataType: "json",
+    })
+        .done(function (response) {
+            if (response.success) {
+                var accounts = '';
+                var oddOrEven = 1;
+                var oddOrEvenText = '';
+                response.accounts.forEach(function (accountData) {
+                    if (oddOrEven % 2 == 1) {
+                        oddOrEvenText = 'odd';
+                    } else {
+                        oddOrEvenText = 'even';
+                    }
+                    accounts += '<tr role="row" class="' + oddOrEvenText + '">' +
+                                '   <th class="sorting_1">' +
+                                '       <div>' +
+                                '           <span>' +
+                                '               <a href="#">' +
+                                '                   <img class="avatarSize" src="assets/images/' + accountData['profile_picture'] + '">' +
+                                '               </a>' +
+                                '           </span>' +
+                                '       </div>' +
+                                '   </th>' +
+                                '   <td> ' + accountData['first_name'] + ' ' + accountData['last_name'] + '</td>' +
+                                '   <td>' + accountData['email'] + '</td>' +
+                                '   <td><span class="' + accountData['dotColor'] + '"><span class="' + accountData['dotClass'] + '"></span>' + accountData['isOnline'] + '</span>' +
+                                '       <button class="btn updateButtonPos" href="#">Update</button>' +
+                                '   </td>' +
+                                '</tr>';
+                    oddOrEven++;
+                });
+                $("#tableBody").append(accounts);
+
+            } else {
+                console.error('Accounts unsuccessfully fetched');
+            }
+            $('#usersTable').DataTable();
+        });
 });
