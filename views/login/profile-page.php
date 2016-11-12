6e5ac1cc8e91fac16c591313166574fea8d50896
teo9898@abv.bg <teo9696@abv.bg>
2016-11-12 19:53:47 +0100

    feature: view own account

---
+++ views/login/profile-page.php
@@ -0,0 +1,71 @@
+<div class="col-xs-9">
+    <div class="jumbotron jumboFix">
+        <div style="border-radius: 10px;background: whitesmoke;">
+            <table class="table table-hover">
+                <tbody>
+                <tr>
+                    <td class="fieldCell" colspan="1" style="width: 25%;">
+                        <img src="assets/images/<?php echo $user_data['profile_picture']; ?>"
+                             alt="<?php echo $user_data['first_name'] . ' ' . $user_data['last_name']; ?>"
+                             title="<?php echo $user_data['first_name'] . ' ' . $user_data['last_name']; ?>"
+                             class="img-thumbnail">
+                    </td>
+                    <td class="fieldCell" colspan="2" style="width: 75%;"><h3><?php echo $user_data['first_name'] . ' ' . $user_data['last_name']; ?></h3></td>
+                </tr>
+                <tr>
+                    <td class="fieldCell" colspan="2"
+                        style="width: 100%;background-color: #ecf0f1;border-radius: 5px; ">
+                        <span class="cb_offline text-success"><span class="fa fa-circle"></span> ONLINE</span>
+                        <span style="margin-left: 20px;"><button name="editProf" id="<?php echo 'editThis'. $user_data['user_id'];?>" class="btn btn-default">Edit profile</button></span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="clearfix">
+            <div class="col-sm-9">
+                <div><table class="table table-hover">
+                    <tbody>
+                    <tr>
+                        <td class="titleCell" style="width: 25%;">
+                            <label for="email_l">Email</label>
+                        </td>
+                        <td id="email_l" class="fieldCell" style="width: 75%;"><?php echo $user_data['email']; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="titleCell" style="width: 25%;">
+                            <label for="phone_l">Mobile nr</label>
+                        </td>
+                        <td id="phone_l" class="fieldCell" style="width: 75%;"><?php echo $user_data['phone']; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="titleCell" style="width: 25%;">
+                            <label for="address_l">Address</label>
+                        </td>
+                        <td id="address_l" class="fieldCell" style="width: 75%;"><?php echo $user_data['address']; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="titleCell" style="width: 25%;">
+                            <label for="code_l">Zip code</label>
+                        </td>
+                        <td id="code_l" class="fieldCell" style="width: 75%;"><?php echo $user_data['zip_code']; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="titleCell" style="width: 25%;">
+                            <label for="city_l">City</label>
+                        </td>
+                        <td id="city_l" class="fieldCell" style="width: 75%;"><?php echo $user_data['city']; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="titleCell" style="width: 25%;">
+                            <label for="status_l">User Status</label>
+                        </td>
+                        <td id="status_l" class="fieldCell" style="width: 75%;"><?php echo $user_data['type']; ?></td>
+                    </tr>
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
