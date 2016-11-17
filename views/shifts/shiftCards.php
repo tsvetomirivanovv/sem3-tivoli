68200f258e1e58ddc17a1ce036a9577c3506f396
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-18 00:24:44 +0100

    feat(cancelShift): add cancel shift modal

--- views/shifts/shiftCards.php
+++ views/shifts/shiftCards.php
@@ -6,5 +6,26 @@
             </div>
         </div>
     </div>
-</div>
 
+    <div class="modal fade" id="cancelShift" role="dialog">
+        <div class="modal-dialog modal-sm">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Cancel shift</h4>
+                </div>
+                <div class="modal-body" >
+                    <p>
+                        Are you sure you want to cancel this shift?
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <input type="submit" value="Confirm" class="btn btn-block btn-lg btn-default cancelShiftButton"
+                           data-dismiss="modal"/>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
