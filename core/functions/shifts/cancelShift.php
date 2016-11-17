cf9ceab2304d19b1a94154c735a35bce9ec83d3f
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-18 00:26:14 +0100

    feat(cancelShift): add cancelShift endpoint (backend functionality)

---
+++ core/functions/shifts/cancelShift.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../../database/connect.php';
+$conn = getConnection();
+
+// CHECK IF THERE IS ERROR
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+if (isset($_POST['shift_id'])) {
+
+    $id = $_POST['shift_id'];
+
+    // BUILD QUERY
+    $query = "UPDATE shifts set canceled = 1 WHERE shift_id = '$id' ";
+    // EXECUTES QUERY
+    $result = $conn->query($query);
+
+    if($result) {
+        $response = array('success' => true);
+    }
+} else {
+    $response = array('success' => false, 'message' => 'Something went wrong!');
+}
+
+echo json_encode($response);
