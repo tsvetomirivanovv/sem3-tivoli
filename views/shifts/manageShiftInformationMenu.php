11e6a27f50d188e42168757c9931411954e27b45
teo9898@abv.bg <teo9696@abv.bg>
2016-11-18 12:52:05 +0100

    feature: shift details view handled

--- views/shifts/manageShiftInformationMenu.php
+++ views/shifts/manageShiftInformationMenu.php
@@ -3,12 +3,14 @@
         <div class="page-header headerFix">
             <h3 class="h3Fix">
                 <span class="titleClass" style="width: 20% "> </span>
+                <?php if (has_access($user_data['user_id'], 'Manager')) { ?>
                 <a class='cancel_shift_glyphicon' style="float: right " href='#'>
                     <div class='glyphicon glyphicon-remove-circle'></div>
                 </a>
                 <a class='edit_shift_glyphicon'  href='#'>
                     <div class='glyphicon glyphicon-edit' style="float: right"></div>
                 </a>
+                <?php } ?>
                 <button class="btn btn-success book-button" style="float: right">Book Now</button>
             </h3>
         </div>
