8cfb9c8df6736386a09a1b076706caa96938a9f1
Dan Lepadatu <lepadatudan@gmail.com>
2016-11-19 22:29:24 +0100

    feat(cancelShift): add cancel modal in shiftDetails page

--- views/shifts/manageShiftInformationMenu.php
+++ views/shifts/manageShiftInformationMenu.php
@@ -3,9 +3,7 @@
         <div class="page-header headerFix">
             <h3 class="h3Fix">
                 <span class="titleClass" style="width: 20% "> </span>
-                <a class='cancel_shift_glyphicon' style="float: right " href='#'>
-                    <div class='glyphicon glyphicon-remove-circle'></div>
-                </a>
+                <a class='cancel_shift_glyphicon cancelShiftBtn' style="float: right " data-toggle='modal' data-target='#cancelShift'></a>
                 <a class='edit_shift_glyphicon'  href='#'>
                     <div class='glyphicon glyphicon-edit' style="float: right"></div>
                 </a>
@@ -33,3 +31,24 @@
     </div>
 </div>
 
+<div class="modal fade" id="cancelShift" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Cancel shift</h4>
+            </div>
+            <div class="modal-body" >
+                <p>
+                    Are you sure you want to cancel this shift?
+                </p>
+            </div>
+            <div class="modal-footer">
+                <input type="submit" value="Confirm" class="btn btn-block btn-lg btn-default cancelShiftButton"
+                       data-dismiss="modal"/>
+            </div>
+        </div>
+
+    </div>
+</div>
