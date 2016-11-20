ee8e201810e7bd842aa4d5469c8f8477db342eee
teo9898@abv.bg <teo9696@abv.bg>
2016-11-20 17:54:44 +0100

    fix: merged conflicts resolved 2

--- assets/js/shifts/shiftCards.js
+++ assets/js/shifts/shiftCards.js
@@ -29,6 +29,16 @@ $.ajax('core/functions/profile/permissions/is-manager.php', {
                         } else if (shiftData['participants_perc'] > 70) {
                             progress_bar_color = 'progress-bar-danger';
                         }
+                        // check if shift is canceled
+                        var status = "";
+                        var cancelBtn = "";
+                        if(parseInt(shiftData['canceled']) === 1) {
+                            status = "<span class='canceledShift'>(Canceled)</span>";
+                        } else {
+                            cancelBtn = "                       <a class='cancel_shift_glyphicon cancel_shift' data-toggle='modal' data-target='#cancelShift'>" +
+                                "                           <div class='glyphicon glyphicon-remove-circle'></div>" +
+                                "                       </a>";
+                        }
                         shifts += "<li style='list-style-type: none'>" +
                             "   <div class='mat_single_event_holder " + bgStyle + "'>" +
                             "       <div class='mat_single_event_holder_inner'>" +
@@ -41,20 +51,20 @@ $.ajax('core/functions/profile/permissions/is-manager.php', {
                             "           </div>" +
                             "       <div class='mat_event_content'>" +
                             "           <div class='mat_event_content_inner'>" +
-                            "               <h4 class='h4_shift_link'><a class='a_link_title_color' href='fullShiftDetails.php' id='" + shiftData['shift_id'] + "'>" + shiftData['title'] + "</a></h4>" +
+                            "               <h4 class='h4_shift_link'><a class='a_link_title_color shiftId' href='fullShiftDetails.php' id='" + shiftData['shift_id'] + "'>" + shiftData['title'] + "</a>" + status + "</h4>" +
                             "                   <div class='mat_event_location'>" +
-                            "                       <strong><a class='a_link_tivoli_location' href='#'>Tivoli Hotel &amp; Congress Center</a>" + " " + parseTimestamp(shiftData['begin']) + "</strong>" +
+                            "                       <strong><a class='a_link_tivoli_location' href='#'>Tivoli Hotel &amp; Congress Center</a> <br> <span class='beginDate'>" + parseTimestamp(shiftData['begin']) + "</span></strong>" +
                             "                   </div>" +
-                            "                   <div class='mat_small mat_booked participants_count'> " + shiftData['participants'] + " out of " + shiftData['max_participants'] + " participants  </div>" +
+                            "                   <div class='mat_small mat_booked participants_count'> " + shiftData['participants'] + " out of <span class='maxPart'>" + shiftData['max_participants'] + "</span> participants  </div>" +
                             "                       <div class='progress_bar_margin'>" +
                             "                           <div class='progress'>" +
                             "                               <div class='progress-bar " + progress_bar_color + "' style='width: " + shiftData['participants_perc'] + "%;'></div>" +
                             "                           </div>" +
                             "                       </div>" +
-                            "                       <span class='mat_small mat_booked closing_date col-xs-10'>Closing date: " + parseTimestamp(shiftData['close']) + "</span>" +
+                            "                       <span class='mat_small mat_booked closing_date col-xs-10'>Closing date: <span class='closeDate'>" + parseTimestamp(shiftData['close']) + "</span></span>" +
                             "                           <div class='mat_event_infoline duty_manager col-xs-8'>" +
                             "                               <span class='mat_small'>" +
-                            "                               <span>Duty manager: " + shiftData['duty_manager'] + " - </span>Category: " + shiftData['category'] + " </span>" +
+                            "                               <span>Duty manager: <span class='dutyMngr'>" + shiftData['duty_manager'] + "</span> - </span>Category: <span class='category'>" + shiftData['category'] + "</span> </span>" +
                             "                           </div>";
 
                         if (!response1.success) {
@@ -65,12 +75,12 @@ $.ajax('core/functions/profile/permissions/is-manager.php', {
                                 "   </div>" +
                                 "</li>";
                         } else {
-                            shifts += "                       <a class='edit_shift_glyphicon' href='#'>" +
+                            shifts +="                     <div class='buttonsWrapper'> " +
+                                "                       <a class='edit_shift_glyphicon openUpdateShift' data-toggle='modal' data-target='#updateShift'>" +
                                 "                           <div class='glyphicon glyphicon-edit'></div>" +
                                 "                       </a>" +
-                                "                       <a class='cancel_shift_glyphicon' href='#'>" +
-                                "                           <div class='glyphicon glyphicon-remove-circle'></div>" +
-                                "                       </a>" +
+                                cancelBtn +
+                                "                    </div>   " +
                                 "                   </div>" +
                                 "               </div>" +
                                 "                   <div style='clear:both'></div>" +
