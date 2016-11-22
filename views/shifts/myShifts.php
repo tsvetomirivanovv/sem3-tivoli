b3fcccd74a2753cd5414de11c75a9f89bf42424d
Tsvetomir Ivanov <tsvetomirivanovv@gmail.com>
2016-11-22 23:40:28 +0100

    Added modal for cancel  in My Shits and also fixed the loading of My Shifts

--- views/shifts/myShifts.php
+++ views/shifts/myShifts.php
@@ -8,3 +8,22 @@
     </div>
 </div>
 
+<div class="modal fade" id="cancelBookingModal" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Cancel booking</h4>
+            </div>
+            <div class="modal-body">
+                <p>Do you want to cancle your booking for this shift?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                <button id="cancelBookingButton" type="button" class="btn btn-default" data-dismiss="modal">Yes</button>
+            </div>
+        </div>
+
+    </div>
+</div>
